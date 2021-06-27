b432ffd56c5ed1e7a4e88e266d80198161d0e912
Sergio T <stakah@gmail.com>
2021-06-27 13:47:10 -0300

    Limpando o codigo

--- ledmatrixclock/src/main.cpp
+++ ledmatrixclock/src/main.cpp
@@ -13,30 +13,6 @@ LedControl lc= LedControl(DIN, CLK, CS,4);
 int leftPin = A0;
 int h,m,s,hs = 0;
 
-byte Cat[8] ={B10001000,
-              B11111000,
-              B10101000,
-              B01110010,
-              B00100100,
-              B01111100,
-              B01111110,
-              B10111100 };
-byte Apple [8]={B01100000,
-                B00010000,
-                B01101100,
-                B11111010,
-                B11111110,
-                B11111110,
-                B01111100,
-                B00101000};
-byte Heart [8]={B01101100,
-                B10010010,
-                B10000010,
-                B10000010,
-                B10000010,
-                B01000100,
-                B00101000,
-                B00010000};
 byte digits[10][8]={
   {B01111000, //0
    B11000100,
@@ -120,7 +96,6 @@ byte digits[10][8]={
    B00000000}
 };
 
-int debug = 1;
 void setup() {
   Serial.begin(9600);
   for (int d=0; d<4; d++) {
@@ -128,17 +103,12 @@ void setup() {
     lc.setIntensity(d,0);
     lc.clearDisplay(d);
   }
-//  m = 39;
 }
 
 void drawNumber(int num, int col, int dots, int offset) {
   int d = num / 10;
   int u = num % 10;
-//    Serial.print("num:");Serial.print(num);
-//    Serial.print(" col:");Serial.print(col);
-//    Serial.print(" d:"); Serial.print(d);
-//    Serial.print(" u:"); Serial.print(u);
-//    Serial.println();
+
   for (int row=0; row<8; row++) {
     int vd = digits[d][row] >> offset;
     int vu = digits[u][row] >> offset;
@@ -147,8 +117,7 @@ void drawNumber(int num, int col, int dots, int offset) {
     }
     lc.setRow(col, row, vd);
     lc.setRow(col-1, row, vu);
-//    Serial.print(" vd:");Serial.print(vd);
-//    Serial.print(" vu:");Serial.println(vu);
+
   }
 }
 
@@ -175,87 +144,5 @@ void simpleClock() {
 }
 
 void loop() {
-//  pictures();
-//  vu();
   simpleClock();
-}
-
-int leftCh = 32;
-int rightCh = 0;
-int maxAna = 0;
-int minAna = 2000;
-
-void drawLine(int row, int val) {
-  byte aux;
-  for (int d=0; d<4; d++) {
-    aux = val>=8 * (d+1) ? 8 : min(val,8 * (d+1))-(8 * d);
-    lc.setRow(d, row, 255 >> (8 - aux));
-    
-  }
-  
-}
-void vu() {
-//  leftCh = random(33);
-//  rightCh = random(33);
-  int anaLeft = analogRead(leftPin);
-  // 24 - x - 335
-  //  0 - y -  32
-  // (335-25)/(32-0) = (x-25)/(y-0)]
-  // y * 32 = 310 * (x-25) => y = 310/32 * (x-25)
-  
-  leftCh = abs(anaLeft - 0) * 348 / 32;
-  rightCh = leftCh;
-
-  if (anaLeft > maxAna) {
-    maxAna = anaLeft;
-    Serial.print("minAna: ");
-    Serial.print(minAna);
-    Serial.print("maxAna: ");
-    Serial.println(maxAna);
-  }
-  else if (anaLeft < minAna) {
-    minAna = anaLeft;
-    Serial.print("minAna: ");
-    Serial.print(minAna);
-    Serial.print("maxAna: ");
-    Serial.println(maxAna);
-  }
-  
-  if (false && debug) {
-  Serial.print("leftPin: ");
-  Serial.print(anaLeft);
-  Serial.print("  leftCh: ");
-  Serial.println(leftCh);
-  delay(30);
-  }
-  
-  drawLine(1, leftCh);
-  drawLine(2, leftCh);
-
-  drawLine(5, rightCh);
-  drawLine(6, rightCh);
-
-//  delayMicroseconds(20);
-   delay(50);
-}
-
-void pictures(){
-  static int c = 0;
-  static int d = 1;
-  static int e = 2;
-  static int f = 3;
-//  do {
-    for(int i=0;i<8;i++) {
-      lc.setRow(d,i,Apple[i]);
-      lc.setRow(e,i,Cat[i]);
-      lc.setRow(f,i,Heart[i]);
-      lc.setRow(c,i,0);
-      delay(200);
-    }
-    delay(5000);
-    c = (c + 1) % 4;
-    d = (d + 1) % 4;
-    e = (e + 1) % 4;
-    f = (f + 1) % 4;
-//  } while (1);
 }
